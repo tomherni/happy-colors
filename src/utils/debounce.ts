53462efac7d1c0cf04d7b07e75e0d2dc47210a45
tomherni <tomherni@gmail.com>
2020-12-12 19:20:09 +0100

    add types for the debouncer

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,14 +1,20 @@
-// @ts-nocheck
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type DebounceCallback = (...args: any[]) => void;
 
 /**
  * Debounce a function based on RAF.
  * @param {Function} callback - Function to debounce
  * @returns {Function} Debounced function
  */
-export function debounce(callback) {
-  let timeout;
+export function debounce<F extends DebounceCallback>(
+  callback: F
+): (this: ThisParameterType<F>, ...args: Parameters<F>) => void {
+  let timeout: number;
 
-  return function debounced(...args) {
+  return function debounced(
+    this: ThisParameterType<F>,
+    ...args: Parameters<F>
+  ): void {
     window.cancelAnimationFrame(timeout);
     timeout = window.requestAnimationFrame(() => callback.apply(this, args));
   };
